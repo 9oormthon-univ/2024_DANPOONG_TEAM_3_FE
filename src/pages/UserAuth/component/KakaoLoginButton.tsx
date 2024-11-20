56a18edcf01ed68f4e7bfb51780c47ac61ca1364
Soomin Lee <soomin@greyboxhq.com>
2024-11-21 00:00:20 +0900

    feat: add handleKakaoLogin

--- src/pages/UserAuth/component/KakaoLoginButton.tsx
+++ src/pages/UserAuth/component/KakaoLoginButton.tsx
@@ -1,13 +1,24 @@
 import KakaoButton from '@/assets/KakaoLoginButton.svg?react';
 import KakaoLoginCharacter from '@/assets/KakaoLoginCharacter.svg?react';
 
+import { KAKAO_URL } from '../constant';
+
 export function KakaoLoginButton() {
     return (
         <div className="flex flex-col justify-center items-center">
             <KakaoLoginCharacter />
-            <button className="bg-transparent focus:outline-none border-0 w-[493px] p-0">
+            <button
+                className="bg-transparent focus:outline-none border-0 w-[493px] p-0"
+                onClick={() => {
+                    handleKakaoLogin();
+                }}
+            >
                 <KakaoButton className="size-fit" />
             </button>
         </div>
     );
+
+    function handleKakaoLogin() {
+        window.location.href = KAKAO_URL;
+    }
 }
