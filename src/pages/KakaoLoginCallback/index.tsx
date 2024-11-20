518670e0070dae4a07b8b99ec2a106ffb9935c25
Soomin Lee <soomin@greyboxhq.com>
2024-11-21 03:28:37 +0900

    feat: add kakao-login

--- src/pages/KakaoLoginCallback/index.tsx
+++ src/pages/KakaoLoginCallback/index.tsx
@@ -10,7 +10,7 @@ export function KakaoLoginCallback() {
         postKakaoLogin(code, () => {
             navigate('/');
         });
-    }, [code]);
+    }, []);
 
     return (
         <div>
--- src/pages/UserAuth/api/postKakaoLogin.ts
+++ src/pages/UserAuth/api/postKakaoLogin.ts
@@ -1,16 +1,41 @@
 import axios from 'axios';
+import qs from 'qs';
 import { KAKAO_CODE_POST_URL } from '../constant';
 
 export const postKakaoLogin = async (code: string, callback: () => void) => {
     try {
         axios
-            .post(KAKAO_CODE_POST_URL, {
-                code: code,
-            })
-            .then((res) => {
-                if (res.data.code === 200) {
-                    callback();
+            .post(
+                KAKAO_CODE_POST_URL,
+                qs.stringify({
+                    grant_type: 'authorization_code',
+                    client_id: import.meta.env.VITE_KAKAO_API_KEY,
+                    redirect_uri: import.meta.env.VITE_KAKAO_REDIRECT_URI,
+                    code: code,
+                }),
+                {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+                    },
                 }
+            )
+            .then((res) => {
+                axios
+                    .post(
+                        'https://kapi.kakao.com/v2/user/me',
+                        {
+                            accessToken: res.data.access_token,
+                        },
+                        {
+                            headers: {
+                                Authorization: `Bearer ${res.data.access_token}`,
+                            },
+                        }
+                    )
+                    .then((res) => {
+                        console.log(res);
+                        callback();
+                    });
             });
     } catch (error) {
         console.error(error);
