0a371258b2f24ed1471d9cd1877e8c02f27ef15d
Soomin Lee <soomin@greyboxhq.com>
2024-11-20 23:34:18 +0900

    feat: add api/callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { UserAuth } from './pages/UserAuth';
+import { KakaoLoginCallback } from './pages/KakaoLoginCallback';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/auth" element={<UserAuth />} />
+                <Route path="/api/callback" element={<KakaoLoginCallback />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/pages/UserAuth/constant/index.ts
@@ -0,0 +1,2 @@
+export const KAKAO_CODE_POST_URL =
+    'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&cliende=W0AxCs6SIAoTRtcfkM-79FWY9r0eVOXFg8n-Xnay4CkbZzc_wrPUDgAAAAQKPCRaAAABkz_YcPctjdRiIM79qQ';
