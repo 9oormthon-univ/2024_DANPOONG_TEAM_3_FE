46ece1eb4dd3dea41a2046cc51feba8d5c7e3a55
Soomin Lee <soomin@greyboxhq.com>
2024-11-23 15:32:41 +0900

    chore: update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { UserAuthPage } from './pages/UserAuth';
 import { KakaoLoginCallback } from './pages/KakaoLoginCallback';
-import { Challenge } from './pages/Challenge';
+import { ChallengePage } from './pages/Challenge';
 import { OverlayProvider } from '@toss/use-overlay';
+import { ActivityDetailPage } from './pages/ActivityDetail';
 
 function App() {
     return (
-        <OverlayProvider>
-            <BrowserRouter>
+        <BrowserRouter>
+            <OverlayProvider>
                 <Routes>
                     <Route path="/auth" element={<UserAuthPage />} />
                     <Route path="/api/callback" element={<KakaoLoginCallback />} />
-                    <Route path="/challenge" element={<Challenge />} />
+                    <Route path="/challenge" element={<ChallengePage />} />
+                    <Route path="/activity/:id" element={<ActivityDetailPage />} />
                 </Routes>
-            </BrowserRouter>
-        </OverlayProvider>
+            </OverlayProvider>
+        </BrowserRouter>
     );
 }
 
